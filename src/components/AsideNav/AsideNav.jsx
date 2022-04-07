3555618bc7043126c16c6d34758a5c944276b078
Tether <tarun.sankhla.prc@gmail.com>
2022-04-07 12:58:03 +0530

    added navlink css

--- src/components/AsideNav/AsideNav.jsx
+++ src/components/AsideNav/AsideNav.jsx
@@ -9,44 +9,51 @@ import { NavLink } from "react-router-dom";
 import { IcOutlineThumbUp } from "../UI/Icons";
 
 function AsideNav() {
+  const getActiveStyle = ({isActive}) =>({
+    color : isActive ?  "#539987" : "",
+    transform: isActive ?  "scale(1.1)" : "",
+    opacity: isActive ? "0.9" : "",
+    backgroundColor: isActive ?  "rgba(0, 0, 0, 0.1)" : "",
+  })
+
   return (
     <div className="AsideNav">
-      <NavLink to="/">
+      <NavLink to="/" style={getActiveStyle} >
         <div className="aside-nav-row">
           <IcTwotoneHome />
           <span className="icon-handle">Home</span>
         </div>
       </NavLink>
 
-      <NavLink to="/explore">
-        <div className="aside-nav-row">
+      <NavLink to="/explore" style={getActiveStyle} className="aside-nav-row">
+        {/* <div > */}
           <IcTwotoneVideoLibrary />
           <span className="icon-handle">Explore</span>
-        </div>
+        {/* </div> */}
       </NavLink>
-      <NavLink to="/playlist">
-        <div className="aside-nav-row">
+      <NavLink to="/playlist" style={getActiveStyle} className="aside-nav-row">
+        {/* <div > */}
           <IcTwotonePlaylistPlay />
           <span className="icon-handle">PlayList</span>
-        </div>
+        {/* </div> */}
       </NavLink>
-      <NavLink to="/likes">
-        <div className="aside-nav-row">
+      <NavLink to="/likes" style={getActiveStyle} className="aside-nav-row">
+        {/* <div > */}
           <IcOutlineThumbUp />
           <span className="icon-handle">Likes</span>
-        </div>
+        {/* </div> */}
       </NavLink>
-      <NavLink to="/watchlater">
-        <div className="aside-nav-row">
+      <NavLink to="/watchlater" style={getActiveStyle} className="aside-nav-row">
+        {/* <div> */}
           <IcTwotoneWatchLater />
           <span className="icon-handle">Watch Later</span>
-        </div>
+        {/* </div> */}
       </NavLink>
-      <NavLink to="/history">
-        <div className="aside-nav-row">
+      <NavLink to="/history" style={getActiveStyle} className="aside-nav-row">
+        {/* <div> */}
           <IcTwotoneManageHistory />
           <span className="icon-handle">History</span>
-        </div>
+        {/* </div> */}
       </NavLink>
     </div>
   );
